224f8e6e763b36049682d9674033f3ae4b82d551
Yngredh <yngredh.cruz@gmail.com>
2024-03-24 20:49:26 -0300

    chore: initial project configuration

---
+++ src/main/java/com/aluratechnicalcase/AluraTechnicalCaseApplication.java
@@ -0,0 +1,13 @@
+package com.aluratechnicalcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraTechnicalCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraTechnicalCaseApplication.class, args);
+	}
+
+}
