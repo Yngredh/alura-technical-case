caea35be670706312fb442de0988f9e208a6b52e
Yngredh <yngredh.cruz@gmail.com>
2024-03-25 00:03:05 -0300

    feat: Add ControllerExceptionHandler for centralized exception handling

---
+++ src/main/java/com/aluratechnicalcase/infrastructure/ControllerExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.aluratechnicalcase.infrastructure;
+
+import com.aluratechnicalcase.domain.exception.UserAlreadyExistException;
+import com.aluratechnicalcase.domain.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(UserAlreadyExistException.class)
+    public ResponseEntity<String> handleUserAlreadyExistException(UserAlreadyExistException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Este usuário já existe");
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Username não encontrado");
+    }
+}
